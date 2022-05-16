e182732800e1d272ef0d5138d56683985cb528e5
Doug A <douganderson444@gmail.com>
2022-05-16 14:41:07 -0300

    add preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess({}),
+
 	kit: {
 		adapter: adapter({
 			pages: 'docs',
