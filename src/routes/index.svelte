bf5a4b70fdaaaa85e9a2ead9ca8e7ebc17ac05b7
Doug A <douganderson444@gmail.com>
2022-05-31 13:44:39 -0300

    add cache

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,15 +2,39 @@
 	import App from '@douganderson444/web3-repl-deploy';
 	import { code_0 } from '$lib/_source';
 
-	let components = [
-		{
-			name: 'App',
-			type: 'svx',
-			source: code_0
+	import { onMount } from 'svelte';
+
+	let components;
+	let mounted;
+
+	onMount(() => {
+		mounted = true;
+		// get draft cache, if any
+		const cache = localStorage.getItem('cache');
+
+		if (cache) {
+			// if cache, load it
+			components = JSON.parse(cache);
+		} else {
+			// otherwise, set default
+			components = [
+				{
+					name: 'App',
+					type: 'svx',
+					source: code_0
+				}
+			];
 		}
-	];
+	});
+
+	// cache local drafts
+	$: if (mounted && components && localStorage) {
+		localStorage?.setItem('cache', JSON.stringify(components));
+	}
 </script>
 
 <main>
-	<App {components} />
+	{#if components}
+		<App bind:components />
+	{/if}
 </main>
