994c0de69037320ec2229dcdef66eb118baec60e
Doug A <douganderson444@gmail.com>
2022-06-17 09:16:22 -0300

    add changable tab

--- src/lib/_source.js
+++ src/lib/_source.js
@@ -5,81 +5,90 @@ export const getAppSvxCode = ({ token }) => {
 	return code_1;
 };
 export const code_0 = `---
-title: Frontmatter can be used
 author: Doug Anderson (@douganderson444)
 
 ---
 
 <script>
-	import { editable } from 'https://cdn.jsdelivr.net/gh/douganderson444/svelte-editable-action@latest/src/lib/editable.js'
-		
-	export let name = 'Darik';
-	let editText = 'Go ahead and edit me'
+  import { editable } from  '@douganderson444/svelte-editable-action@latest'
+	import List from '@douganderson444/listavelte@0.1.6'
+	import Changable from './Changable.svelte'
+	
+	export let name = 'Douglas';
+	export let lastName = 'Anders'
+	
+	export let editableTitle = 'Click to edit My first Template'
+	export let fancyEditText = 'Click me to fancy edit, watch me update'
+	
+	const makeProp = varObj => ({ key: Object.keys(varObj)[0], value: varObj[Object.keys(varObj)[0]]})
+
 </script>
 
-<h1>{title}</h1>
+<h1>
+<Changable prop={makeProp({editableTitle})} on:change={e=>console.log(e.detail)}></Changable> {name} {lastName}
+</h1>
 
-# What can you write {name}?
-
-## Markdown Syntax works
-
-### Make text Smaller by adding #s
-
-<div use:editable on:change={e=>console.log(e)}>
-	{editText}
+<div use:editable on:change={(e) => fancyEditText = e.detail } >
+	<h2>{fancyEditText}</h2>
 </div>
 
-{editText}
+### Markdown Syntax works
+
+#### Let's import a TODO Component:
+
+<!-- use on:change to pass the properies up to the parent component -->
+<List on:change></List>
 
 _Written by:_
 
 {author}
 
 <style>
-	h1 {
-		color: red;
+	:root {
+		background-color: #fafafa;	
 	}
+
 	
 	h2 {
 		color: blue
 	}
+		
+	h4 {
+		color: green
+	}
+	h1 {
+		color: red;
+		font-family: 'Luckiest Guy', sans-serif;
+		text-transform: uppercase;
+	}
+
+	@font-face {
+		font-family: 'Luckiest Guy';
+		font-style: normal;
+		font-weight: 400;
+		src: url(https://fonts.gstatic.com/s/luckiestguy/v17/_gP_1RrxsjcxVyin9l9n_j2hTd52.woff2)
+			format('woff2');
+		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
+			U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+	}
 </style>
 `;
 
-export const code_1 = `---
-title: Web3 Tokenization REPL
-author: DougAnderson444
-date: 1 September 2021
-endpoint: devnet
-tokenKey: BFVp8duWGJTyEDzaDX9sVzJCDnikXp3LLpyuJ29pZLhv
-
----
-
-<script>
-import Solana from './Solana.svelte';
-import Layout from './Layout.svelte';
-import Asset from './Asset.svelte';
-
-let account = "2kNoPZhth7i9nRBQikdHUUYP9vx9FqPQP66zaQFDEtoU"; // example
+export const code_1 = `<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	
+	// pass in the item you want to monitor changes on
+	export let prop = { 
+		key: 'editableTitle', 
+		value: 'Some content to change' 
+	}
+	// dispatch the change event when the content is edited
+	$: if (prop.value) dispatch('change', { [prop.key]: prop.value });
 
 </script>
 
-<Layout {title} {author} {date} >
-	<Solana {account} {tokenKey} {endpoint} >
-		<Asset />
-	</Solana>
-
-	### Make your own NFT or zkap gateway:
-
-	**This REPL:** 
-
-	1. Wraps your asset in an Arweave display app which checks for the presence of the token in the requesting account. App Tokenization!
-	
-	2. The display app is saved in Arweave, thus the asset lives forever! The token is Solana SPL, so access lives forever! Arweave Tokens coming soon
-	
-	3. Don't hold the token? You don't have access or ownership!
-
-</Layout>
+<span contenteditable bind:textContent={prop.value}></span>
 `;
 
 export const code_2 = `<script>
